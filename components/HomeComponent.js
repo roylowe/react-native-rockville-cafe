ac155ea045b0b7a98c024974ed39cc42d932ea87
RoyLowe <roylowe96@gmail.com>
2021-09-13 17:54:51 -0400

    tried adding content to home, error

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,35 @@
+import { interopRequireDefault } from 'jest-util';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { SPECIALS } from '../shared/specials';
+
+function RenderItem({item}) {
+    if (item) {
+        return(
+            <Card
+                featuredTitle={item.name}
+                image={interopRequireDefault('./images/eggPlateSpecial.jpeg')}>
+                <Text
+                    style={{margin: 10}}>
+                    {item.description}
+                </Text>
+
+            </Card>
+        );
+    }
+    return <View />;
+}
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            specials: SPECIALS
+        };
+
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -9,9 +37,13 @@ class Home extends Component {
 
     render() {
         return (
+            /*{ <ScrollView>
+                <RenderItem
+                    item={this.state.specials.filter(special => special.featured)[0]} />
+            </ScrollView> }*/
             <View>
                 <Text>Home Component</Text>
-            </View>
+            </View> 
         );
     }
 }

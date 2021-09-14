e6c106248ab947c584d9b0815e72226cf8cacd15
RoyLowe <roylowe96@gmail.com>
2021-09-13 21:48:39 -0400

    Home page working

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,7 +3,33 @@ import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import { SPECIALS } from '../shared/specials';
 
+function RenderItem({item}) {
+    if (item) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                image={require('./images/eggPlateSpecial.jpeg')}
+            >
+                <Text style={{margin: 10}}>
+                    {item.description}
+                </Text>
+                <Text>inside render item function</Text>
+            </Card>
+            
+        );
+    }
+    return <View />;
+}
+
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            specials: SPECIALS
+        };
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -11,9 +37,16 @@ class Home extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <RenderItem
+                    item={this.state.specials.filter(special => special.featured)[0]} />
+
+                <Card>
+                    <Text>Come in...sit down... enjoy a home cooked meal!</Text>
+                </Card>
+            </ScrollView>
+
+            
         );
     }
 }
--- shared/specials.js
+++ shared/specials.js
@@ -4,20 +4,23 @@ export const SPECIALS = [
         name: 'Breakfast',
         image: 'assets/images/eggPlateSpecial.jpeg',
         description: "2 eggs served with toast spring greens",
-        cost: "$5.99"
+        cost: "$5.99",
+        featured: false
     },
     {
         id: 1,
         name: 'Lunch',
         image: 'assets/images/lunchSpecial.jpeg',
         description: "tuna salad with lettuce, onions, served on wheat bread",
-        cost: "$8.99"
+        cost: "$8.99",
+        featured: false
     },
     {
         id: 2,
         name: 'Dinner',
         image: 'assets/images/ribDinner.jpeg',
         description: "prime rib with fries, tomatoes and pickles",
-        cost: "$19.99"
+        cost: "$19.99",
+        featured: true
     }
 ]
